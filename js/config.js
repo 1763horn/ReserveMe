207f46394c031264df475439cec6d573dcdfbb3f
Ryan Meyer <ryan.m.meyer@outlook.com>
2021-10-25 13:53:46 -0400

    Edited the cognito config file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_kIx8rNQeO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1khfisvgn9sa0p0fja2fp1710b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://feature-ryan-test-branch.d1xmblrrmhoiux.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
