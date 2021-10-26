0cd5ea0dbcb8d2aa4a39f6e186573c9944b5acb7
Ryan Meyer <56931480+ryan-m-meyer@users.noreply.github.com>
2021-10-26 11:19:25 -0400

    Updated to use a separate app client.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_kIx8rNQeO', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1khfisvgn9sa0p0fja2fp1710b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6s4an2dcr1j62mbeu9me157eq4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
