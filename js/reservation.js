e6d46071f856b7bb356197ba4d6ba323dc6947ab
Ryan Meyer <ryan.m.meyer@outlook.com>
2021-10-27 13:55:25 -0400

    Updating the reservation page

---
+++ js/reservation.js
@@ -0,0 +1,41 @@
+/*global WildRydes _config*/
+
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
+
+(function reservationScopeWrapper($) {
+    function requestReservation(day,hour,full_name,phone,persons) {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/reservations',
+            data: JSON.stringify({
+                Reservation:{
+                    FullName: full_name,
+                    PhoneNumber: phone,
+                    PartySize: persons,
+                    Day: day,
+                    Hour: hour
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+            }
+        });
+    }
+
+
+    // Register click handler for #request button
+    $(function onDocReady() {
+        $('#submit').click(handleRequestClick);
+    });
+
+    function handleRequestClick(event) {
+        event.preventDefault();
+        requestReservation(day,hour,full_name,phone,persons);
+    }
+
+}(jQuery));
